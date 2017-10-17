5b2ab4debdf29bc3dd0d58a94ffd1c22e2611f99
prakash <ptotta@archsystemsinc.com>
2017-10-16 23:53:25 -0400

    updated

---
+++ qamservices/src/main/java/com/archsystemsinc/qam/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.archsystemsinc.qam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.archsystemsinc.qam.model.Address;
+
+/**
+ */
+public interface AddressRepository extends JpaRepository<Address, Long>{
+}
---
+++ qamservices/src/main/java/com/archsystemsinc/qam/repository/SocialMediaRepository.java
@@ -0,0 +1,10 @@
+package com.archsystemsinc.qam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.archsystemsinc.qam.model.SocialMedia;
+
+/**
+ */
+public interface SocialMediaRepository extends JpaRepository<SocialMedia, Long>{
+}
