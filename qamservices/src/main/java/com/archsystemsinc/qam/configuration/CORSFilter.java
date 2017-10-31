b8e19e44b28dafa1f8ad7986b7537ceaa52dd246
prakash <ptotta@archsystemsinc.com>
2017-10-31 17:47:12 -0400

    updated

--- qamservices/src/main/java/com/archsystemsinc/qam/configuration/CORSFilter.java
+++ qamservices/src/main/java/com/archsystemsinc/qam/configuration/CORSFilter.java
@@ -17,8 +17,18 @@ public class CORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "X-requested-with, Content-Type,authorization");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "X-requested-with, Content-Type");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+	    
+		/*
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+	    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	    res.addHeader("Access-Control-Allow-Headers", "Content-Type, authorization");
+	    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	    */
 		chain.doFilter(req, res);
 	}
 
